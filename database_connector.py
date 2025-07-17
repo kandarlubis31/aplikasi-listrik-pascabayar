40381834180b84f1c2a1e049acb249c50c7ba00b
Aulia Iskandar Lubis <auliaiskandarlubis31@gmail.com>
2025-07-17 17:57:56 +0700

    modified:   database_connector.py

--- database_connector.py
+++ database_connector.py
@@ -4,9 +4,9 @@ def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host="localhost",
-            user="aulia31",
-            password="kandar31",
-            database="aulia31$bayarlistrik_db"
+            user="root",
+            password="",
+            database="bayarlistrik_db"
         )
         return conn
     except mysql.connector.Error as err:
